fbbc72ff1824d15cf6170dc2bff95aeb728e9b66
kevinamatte <81264481+kevinamatte@users.noreply.github.com>
2023-05-29 14:55:20 -0600

    Add files via upload

---
+++ Add-ServersToSCCMCollection.ps1
@@ -0,0 +1,7 @@
+﻿$ServerList = Read-Host -Prompt "Enter path to list of server names"
+$CollectionId = Read-Host -Prompt "Enter the Collection ID to add the servers to"
+$servers = Get-Content $ServerList
+
+Foreach ($s in $servers){
+    $i = Get-CMDevice -Name $s
+    Add-CMDeviceCollectionDirectMembershipRule -CollectionId $CollectionId -ResourceId $i.Id}
