0b759af332ea476927bfceb16d0a4d6ac76006e2
kevinamatte <81264481+kevinamatte@users.noreply.github.com>
2023-05-30 09:37:56 -0600

    Update Add-ServersToSCCMCollection.ps1

--- Add-ServersToSCCMCollection.ps1
+++ Add-ServersToSCCMCollection.ps1
@@ -1,7 +1,10 @@
-﻿$ServerList = Read-Host -Prompt "Enter path to list of server names"
+# note: This script cannot be run as-is from SCCM powershell due to execution policies. 
+# Copy and paste the contents of the script into the SCCM PowerShell window.
+
+$ServerList = Read-Host -Prompt "Enter path to list of server names"
 $CollectionId = Read-Host -Prompt "Enter the Collection ID to add the servers to"
 $servers = Get-Content $ServerList
 
 Foreach ($s in $servers){
     $i = Get-CMDevice -Name $s
-    Add-CMDeviceCollectionDirectMembershipRule -CollectionId $CollectionId -ResourceId $i.Id}
+    Add-CMDeviceCollectionDirectMembershipRule -CollectionId $CollectionId -ResourceId $i.ResourceId}
